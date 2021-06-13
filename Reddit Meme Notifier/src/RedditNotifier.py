8a3add072406c4d51c80e841bebf1d343b1d158f
Kai-Tay <84640176+Kai-Tay@users.noreply.github.com>
2021-06-13 19:20:25 +0800

    Add files via upload

--- Reddit Meme Notifier/src/RedditNotifier.py
+++ Reddit Meme Notifier/src/RedditNotifier.py
@@ -1,17 +1,31 @@
 from bs4 import BeautifulSoup
 import requests
-import pync
 import schedule
 import time
 import PickupLines as pl
 import random
-
+#Importing libraries based on Mac/Windows
+try:
+    import pync
+except:
+    from win10toast_click import ToastNotifier
+    import webbrowser
 
 def job():
     pickup_line = random.choice(pl.sentences)
     #Starting Up + Pickup Line!
-    pync.notify('\n' + pickup_line, title='MEME OF THE HOUR?! STARTING UP!', contentImage='https://i.kym-cdn.com/entries/icons/original/000/013/564/doge.jpg')
-    time.sleep(5)
+    try:
+        pync.notify('\n' + pickup_line, title='MEME OF THE HOUR?! STARTING UP!', contentImage='https://i.kym-cdn.com/entries/icons/original/000/013/564/doge.jpg')
+        time.sleep(5)
+    except:
+        toaster = ToastNotifier()
+        toaster.show_toast(
+            "MEME OF THE HOUR?! STARTING UP!", # title
+            '\n' + pickup_line, # messages
+            duration=4, # for how many seconds toast should be visible; None = leave notification in Notification Center
+            threaded=True, # True = run other code in parallel; False = code execution will wait till notification disappears
+            icon_path="doge.ico"
+            )
     #Retrieving from r/meme page
     link = "https://www.reddit.com/r/memes/top/?t=hour"
     headers = {'User-Agent': 'Mozilla/5.0'}
@@ -35,18 +49,32 @@ def job():
         #URL Link
         for url_tag in details.find_all("a", attrs = {'data-click-id' : "body"}):
             url = url_tag["href"]
+            url = "http://reddit.com" + url
 
-    #Notification for Mac
     try:
-        pync.notify(title, title='MEME OF THE HOUR?!?!', contentImage=img, open="http://reddit.com" + url)
+        #Notification for Mac
+        try:
+            pync.notify(title, title='MEME OF THE HOUR?!?!', contentImage=img, open=url)
+        except:
+            pync.notify(title, title='MEME OF THE HOUR?!?!', open=url)
+
     except:
-        pync.notify(title, title='MEME OF THE HOUR?!?!', open="http://reddit.com" + url)
-
-    #Notification for Windows
-
+        def open_url():
+            webbrowser.open_new(url)
+        #Notification for Windows
+        toaster = ToastNotifier()
+        toaster.show_toast(
+            title, # title
+            "Click to open MEME! >>", # message
+            duration=5, # for how many seconds toast should be visible; None = leave notification in Notification Center
+            threaded=True, # True = run other code in parallel; False = code execution will wait till notification disappears
+            callback_on_click=open_url,
+            icon_path="doge.ico"
+        )
 
 #Starts the process
 job()
+
 schedule.every(1).hours.do(job)
 while True:
     schedule.run_pending()
